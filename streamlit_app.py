deaab65154ee788150088f119b6e01072f9675ec
Daniel Nduka <danielnduka@Daniels-MacBook-Pro.local>
2023-10-15 12:46:42 -0500

    Load the motor vechicle collision data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,28 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("Hello World!")
+
+DATA_URL = (
+    "/Users/danielnduka/Downloads/Motor_Vehicle_Collisions_-_Crashes (1).csv"
+)
+
+st.title("Motor Vehicle Collisions in New York City")
+st.markdown("This application is a streamlit dashboard that can be used to analyze motor collision in NYC")
+
+@st.cache_data(persist=True)
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows, parse_dates=[['CRASH_DATE','CRASH_TIME']])
+    data.dropna(subset=['LATITUDE','LONGITUDE'],inplace=True)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns',inplace=True)
+    data.rename(columns={'crash_data_time': 'date/time'},inplace=True)
+    return data
+
+data=load_data(100000)
+
+if st.checkbox("Show Raw Data", False):
+    st.subheader('Raw Data')
+    st.write(data)
+
 
