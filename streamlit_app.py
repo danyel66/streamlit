c080fc1712b24a0b273f50cab602f88c2f54fc0b
Daniel Nduka <danielnduka@Daniels-MacBook-Pro.local>
2023-10-15 14:42:28 -0500

    Plot Filtered Data on a 3D Interacrtive Map

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pydeck as pdk
+
 
 
 DATA_URL = (
@@ -30,6 +32,21 @@ st.map(data.query("injured_persons >= @injured_people")[["latitude","longitude"]
 st.header("How many collisions occur during a given time of day?")
 hour = st.slider("Hour to look at", 0, 23)
 data = data[data['crash_date_crash_time'].dt.hour == hour]
+
+st.header("Vehicle collisions between %i:00 and %i:00" % (hour, (hour + 1) % 24))
+midpoint = (np.average(data['latitude']), np.average(data['longitude']))
+
+st.write(pdk.Deck(
+    map_style="mapbox://styles/mapbox/light-v9",
+    initial_view_state={
+        'latitude': midpoint[0],
+        'longitude': midpoint[1],
+        'zoom': 11,
+        'pitch': 50,
+    },
+)
+
+)
 
 if st.checkbox("Show Raw Data", False):
     st.subheader('Raw Data')
